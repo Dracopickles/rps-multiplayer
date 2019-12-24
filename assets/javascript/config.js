44d146336331276b94f48b62a6036a5974052591
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-12-23 18:53:30 -0800

    added alot of stuff working out firebase kinks

---
+++ assets/javascript/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDZfZoBkexvx6ej06A_H1_LPD3DDP0jGHM",
+  authDomain: "rps-multiplayer-a43e0.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-a43e0.firebaseio.com",
+  projectId: "rps-multiplayer-a43e0",
+  storageBucket: "rps-multiplayer-a43e0.appspot.com",
+  messagingSenderId: "952993414297",
+  appId: "1:952993414297:web:82a73c394b0edc86eb4c08"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
